02a7cc68b65ebcc170c608ce8eef0d2dd200f987
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-13 15:49:42 +0200

    start php-oop-1

---
+++ index.php
@@ -0,0 +1,30 @@
+<!--  Create un file index.php in cui:
+- è definita una classe ‘Movie’
+    => all’interno della classe sono dichiarate delle variabili d’istanza
+    => all’interno della classe è definito un costruttore
+    => all’interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+<?php
+class Movie {
+    public $title;
+    public $year;
+    public $runningTime;
+    public $language;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+
+    </div>
+</body>
+</html>
